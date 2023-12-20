0dc47fc7bc9bbeebdb1b03fadf89b0cec96585d2
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-12-20 14:16:14 +0800

    wip

--- routes/spa.php
+++ routes/spa.php
@@ -63,6 +63,8 @@ Route::post('/logout-user', function (Request $request) {
 
 
 Route::post('/upload-file', [FileController::class , 'uploadFile'])->name('upload-file');
+
+
 Route::prefix('data-collection')->name('data-collection.')->controller(DataCollectionController::class)->group(function(){
     
     Route::post('create', 'create' )->name('create');
