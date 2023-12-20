46fb281b7f4328bef575b6d5030ce046971575b1
GabJeungSook <gicawalo@gmail.com>
2023-12-20 19:50:39 +0800

    added google maps

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('/')
             ->login()
             ->colors([
                 'primary' => "#1e40af",
@@ -37,8 +37,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
---
+++ resources/views/livewire/dashboard-map.blade.php
@@ -0,0 +1,47 @@
+<div class="h-full border-2 border-gray-600 rounded-sm">
+    <style>
+        #map {
+            height: 600px; /* Set a specific height for the map container */
+            width: 100%; /* Set width to fill the available space */
+        }
+    </style>
+    <div id="map">ss</div>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAyBh1oNe0wqgv4NBQ3AjbxGYIh5mvl9YU&callback=initMap" async></script>
+    <script>
+        let map, activeInfoWindow, markers = [];
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: {
+                    lat: 6.117278,
+                    lng: 125.171583,
+                },
+                zoom: 15,
+                styles: [
+                    {
+                        featureType: "poi",
+                        stylers: [
+                            { visibility: "off" } // Hide points of interest (landmarks, parks, etc.)
+                        ],
+                    },
+                    // Add more styling options if needed
+                ],
+
+            });
+                // Define the coordinates for the marker
+                    const markerCoordinates = {
+                    lat: 6.117278,
+                    lng: 125.171583,
+                };
+
+                // Create a marker and set its position on the map
+                const marker = new google.maps.Marker({
+                    position: markerCoordinates,
+                    map: map,
+                    title: 'Marker Title' // Optional: Add a title to the marker
+                });
+        }
+    </script>
+
+</div>
