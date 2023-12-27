5a3cf6a7aad7e48bf9a617205d05c70cd49e90d4
GabJeungSook <gicawalo@gmail.com>
2023-12-27 08:45:17 +0800

    changes

---
+++ vendor/filament/filament/resources/views/pages/dashboard.blade.php
@@ -0,0 +1,18 @@
+<x-filament-panels::page class="fi-dashboard-page">
+    @include('livewire.dashboard-map')
+    {{-- <livewire:dashboard-map /> --}}
+    @if (method_exists($this, 'filtersForm'))
+        {{ $this->filtersForm }}
+    @endif
+
+    <x-filament-widgets::widgets
+        :columns="$this->getColumns()"
+        :data="
+            [
+                ...(property_exists($this, 'filters') ? ['filters' => $this->filters] : []),
+                ...$this->getWidgetData(),
+            ]
+        "
+        :widgets="$this->getVisibleWidgets()"
+    />
+</x-filament-panels::page>
